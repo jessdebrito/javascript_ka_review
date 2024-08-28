54442841f0645267fa5e4a684b2cbd91adfca569
jessdebrito <debrito.herself@gmail.com>
2024-08-28 18:37:44 -0300

    .

---
+++ 09-function/script .js
@@ -0,0 +1,13 @@
+// FUNÇÃO
+
+// - Organizar um bloco de código em um conjunto fechado
+// - Permitir que o código seja dividido em partes menores
+// - Possibilitar reutilizar o código
+
+function formataApresentacao() {
+    const nome = "Thiago";
+    const sobrenome = "Koman";
+    const modulo = "M0";
+
+console.log(`O nome completo do instrutor é: ${nome} ${sobrenome}, e ele trabalha no módulo ${modulo}`)
+}
